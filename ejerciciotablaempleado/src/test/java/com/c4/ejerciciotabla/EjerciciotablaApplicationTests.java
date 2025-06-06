f3a57ea106ced7bb17e4f6af875722ea9fae0c50
XxtrauxX <aaronjones@gmail.com>
2025-06-05 19:44:13 -0500

    subiendo

---
+++ ejerciciotablaempleado/src/test/java/com/c4/ejerciciotabla/EjerciciotablaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c4.ejerciciotabla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjerciciotablaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
